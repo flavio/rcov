ed6051dc49a34b91aa86de487e4c965dd9c5898f
Flavio Castelli <fcastelli@novell.com>
2009-04-17 15:57:47 +0200

    added xml output support for code coverage

--- lib/rcov/report.rb
+++ lib/rcov/report.rb
@@ -1264,6 +1264,77 @@ class RubyAnnotation < Formatter # :nodoc:
     end
 end
 
+class XMLCoverage < Formatter # :nodoc:
+    require 'fileutils'
+
+    DEFAULT_OPTS = {:color => false, :fsr => 30, :destdir => "coverage",
+                    :callsites => false, :cross_references => false,
+                    :validator_links => true, :charset => nil
+                   }
+    def initialize(opts = {})
+      options = DEFAULT_OPTS.clone.update(opts)
+      super(options)
+      @dest = options[:destdir]
+      @color = options[:color]
+      @fsr = options[:fsr]
+      @do_callsites = options[:callsites]
+      @do_cross_references = options[:cross_references]
+      @span_class_index = 0
+      @charset = options[:charset]
+    end
+
+    def execute
+      summary = Document.new
+      coverage_element = Element.new "code_coverage"
+
+      
+      coverage_element.add_attributes( { "total_coverage" => self.total_coverage.to_s,
+                                "num_lines" => self.num_lines.to_s,
+                              "num_code_lines" => self.num_code_lines.to_s})
+
+      each_file_pair_sorted do |filename, fileinfo|
+        file_element = Element.new("file")
+        name = file_element.add_element("name")
+        name.text = filename
+
+        lines_element = Element.new("lines")
+        fileinfo.num_lines.times do |i|
+            line_element = Element.new("line")
+            code = line_element.add_element("code")
+            code.text = fileinfo.lines[i].chomp
+
+            count = line_element.add_element("count")
+            count.text = fileinfo.counts[i]
+
+            marked = line_element.add_element("marked")
+            case fileinfo.coverage[i]
+            when true
+              marked.text = "marked"
+            when :inferred
+              marked.text = "inferred"
+            else
+              marked.text = "uncovered"
+            end
+
+            lines_element.elements << line_element
+        end
+        file_element.elements << lines_element
+
+        file_element.add_attributes( { "total_coverage" => fileinfo.total_coverage.to_s,
+                              "num_lines" => fileinfo.num_lines.to_s,
+                              "num_code_lines" => fileinfo.num_code_lines.to_s})
+
+        coverage_element.elements << file_element
+      end
+      summary.elements << coverage_element
+
+      FileUtils.mkdir_p @dest
+      File.open(File.join(@dest, "coverage.xml"),'w') {|file| file.write(summary) }
+      puts "created #{File.join(@dest, "coverage.xml")}"
+    end
+end
+
+
 
 end # Rcov
 
